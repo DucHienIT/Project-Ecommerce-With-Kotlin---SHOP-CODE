e5e8363e7bc0fe499ab0ef6f987192df6cc94820
123Thanhmy <102036805+123Thanhmy@users.noreply.github.com>
2023-05-14 00:43:25 +0700

    Update AddCartDataSource.kt

--- app/src/main/java/com/example/ecommerce/repository/datasource/AddCartDataSource.kt
+++ app/src/main/java/com/example/ecommerce/repository/datasource/AddCartDataSource.kt
@@ -1,14 +1,14 @@
 package com.example.ecommerce.repository.datasource
 
-import com.example.ecommerce.model.AddCart
+import com.example.ecommerce.model.AddCartMessage
+import com.example.ecommerce.model.CartItem
 import io.reactivex.rxjava3.core.Single
 
 // Interface AddCartDataSource mô tả các phương thức cần thiết để thực hiện hoạt động thêm mặt hàng vào giỏ hàng
 interface AddCartDataSource {
     fun addCart(
-        access_token: String,
-        id: Int,
-        count: Int,
-        price: Int
-    ): Single<List<AddCart>>
+        id:Int,
+        access_token: String
+
+    ): Single<AddCartMessage>
 }
