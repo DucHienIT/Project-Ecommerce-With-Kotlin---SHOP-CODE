9a9241a3e196bfa138870ae57c503d7226511971
123Thanhmy <102036805+123Thanhmy@users.noreply.github.com>
2023-05-14 00:28:10 +0700

    Update AddCartRepository.kt

--- app/src/main/java/com/example/ecommerce/repository/AddCartRepository.kt
+++ app/src/main/java/com/example/ecommerce/repository/AddCartRepository.kt
@@ -1,6 +1,6 @@
 package com.example.ecommerce.repository
 
-import com.example.ecommerce.model.AddCart
+import com.example.ecommerce.model.CartItem
 import io.reactivex.rxjava3.core.Single
 
 // Interface AddCartRepository mô tả phương thức addCart() để thêm mặt hàng vào giỏ hàng
@@ -10,5 +10,5 @@ interface AddCartRepository {
         id: Int,
         count: Int,
         price: Int
-    ): Single<List<AddCart>>
+    ): Single<List<CartItem>>
 }
