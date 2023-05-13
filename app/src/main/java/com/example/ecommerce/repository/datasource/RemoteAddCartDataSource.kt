de68173116147c7fd87f1dff762ca41276118cca
123Thanhmy <102036805+123Thanhmy@users.noreply.github.com>
2023-05-14 00:43:02 +0700

    Update RemoteAddCartDataSource.kt

--- app/src/main/java/com/example/ecommerce/repository/datasource/RemoteAddCartDataSource.kt
+++ app/src/main/java/com/example/ecommerce/repository/datasource/RemoteAddCartDataSource.kt
@@ -1,17 +1,16 @@
 package com.example.ecommerce.repository.datasource
 
-import com.example.ecommerce.model.AddCart
+import com.example.ecommerce.model.AddCartMessage
+import com.example.ecommerce.model.CartItem
 import com.example.ecommerce.network.ApiService
 import io.reactivex.rxjava3.core.Single
 
 class RemoteAddCartDataSource(private val apiService: ApiService) : AddCartDataSource {
     override fun addCart(
-        access_token: String,
-        id: Int,
-        count: Int,
-        price: Int
-    ): Single<List<AddCart>> =
-        apiService.addCart(access_token, id, count, price)
+        id:Int,
+        access_token: String
+    ): Single<AddCartMessage> =
+        apiService.addCart(id,access_token)
 
 
 }
