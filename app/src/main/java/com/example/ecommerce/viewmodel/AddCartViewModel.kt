2633fda63fbc1306dfed0e140aa3d582711e15dc
123Thanhmy <102036805+123Thanhmy@users.noreply.github.com>
2023-05-14 01:07:58 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/ecommerce/viewmodel/AddCartViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.ecommerce.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import com.example.ecommerce.model.AddCartMessage
+import com.example.ecommerce.model.AddFavorite
+import com.example.ecommerce.model.CartItem
+import com.example.ecommerce.repository.AddCartRepository
+import com.example.ecommerce.utils.BaseViewModel
+import com.example.ecommerce.utils.Observer
+import com.example.ecommerce.utils.singleHelper
+import retrofit2.http.Header
+import java.math.BigDecimal
+
+
+// Lớp AddFavoriteViewModel là một ViewModel trong kiến trúc MVVM được sử dụng để xử lý dữ liệu và logic liên quan đến hành động thêm sản phẩm vào danh sách yêu thích của người dùng.
+class AddCartViewModel(private val addCartRepository: AddCartRepository, ) : BaseViewModel() {
+    // đối tượng MutableLiveData để truyền dữ liệu giữa lớp ViewModel và View (Activity hoặc Fragment)
+    val addCartLiveData = MutableLiveData<AddCartMessage>()
+    // Phương thức để thêm sản phẩm vào danh sách yêu thích
+    fun addCart(id: Int,access_token: String) {
+        // Đặt giá trị của đối tượng progressbarLiveData là true để hiển thị tiến trình đang xử lý
+        progressbarLiveData.value = true
+        addCartRepository.addCart(id,access_token)
+            .singleHelper()
+            // Không quan tâm đến kết quả trả về khi hành động kết thúc, đặt giá trị của progressbarLiveData là false để ẩn tiến trình
+            .doFinally {
+                progressbarLiveData.value = false
+            }
+            // Xử lý kết quả trả về khi thêm sản phẩm vào danh sách yêu thích thành công
+            .subscribe(object : Observer<AddCartMessage>(compositeDisposable) {
+                override fun onSuccess(t: AddCartMessage) {
+                    addCartLiveData.value = t!!
+                }
+
+            })
+    }
+}
