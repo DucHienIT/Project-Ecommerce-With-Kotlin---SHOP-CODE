dba2402e9983526e223d78a8cb67bad123e09cc4
123Thanhmy <102036805+123Thanhmy@users.noreply.github.com>
2023-05-14 00:57:39 +0700

    Update CartFragment.kt

--- app/src/main/java/com/example/ecommerce/ui/fragment/cart/CartFragment.kt
+++ app/src/main/java/com/example/ecommerce/ui/fragment/cart/CartFragment.kt
@@ -1,14 +1,28 @@
 package com.example.ecommerce.ui.fragment.cart
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ecommerce.R
+import com.example.ecommerce.ui.adapter.CartListAdapter
 import com.example.ecommerce.utils.Fragment
+import com.example.ecommerce.utils.TokenHolder
+import com.example.ecommerce.viewmodel.CartViewModel
+import com.example.ecommerce.viewmodel.LoginViewModel
+import kotlinx.android.synthetic.main.fragment_cart.*
+import kotlinx.android.synthetic.main.fragment_category.*
+import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
 
 class CartFragment : Fragment() {
-
+    private val cartViewModel: CartViewModel by viewModel()
+    var id: Int? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +30,22 @@ class CartFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cart, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (TokenHolder.access_token!= null) {
+            cartViewModel.cartLiveData.observe(viewLifecycleOwner) {
+                val cartAdapter: CartListAdapter by inject { parametersOf(it) }
+
+                cart.adapter = cartAdapter
+                cart.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            }
+
+
+        }
+
     }
 
 
