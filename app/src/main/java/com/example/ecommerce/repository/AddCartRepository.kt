a805d9bda954d829021fda88dbeded91c22d074f
123Thanhmy <102036805+123Thanhmy@users.noreply.github.com>
2023-05-14 00:44:31 +0700

    Update AddCartRepository.kt

--- app/src/main/java/com/example/ecommerce/repository/AddCartRepository.kt
+++ app/src/main/java/com/example/ecommerce/repository/AddCartRepository.kt
@@ -1,14 +1,13 @@
 package com.example.ecommerce.repository
 
+import com.example.ecommerce.model.AddCartMessage
 import com.example.ecommerce.model.CartItem
 import io.reactivex.rxjava3.core.Single
 
 // Interface AddCartRepository mô tả phương thức addCart() để thêm mặt hàng vào giỏ hàng
 interface AddCartRepository {
     fun addCart(
-        access_token: String,
-        id: Int,
-        count: Int,
-        price: Int
-    ): Single<List<CartItem>>
+        id:Int,
+        access_token: String
+    ): Single<AddCartMessage>
 }
