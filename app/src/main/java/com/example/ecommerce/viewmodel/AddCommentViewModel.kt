26df277d58f70b326074a82d0883a9e326ce5c28
MinhQuan21112002 <93760661+MinhQuan21112002@users.noreply.github.com>
2023-05-14 11:51:31 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/ecommerce/viewmodel/AddCommentViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.ecommerce.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import com.example.ecommerce.model.Comment
+import com.example.ecommerce.repository.AddCommentRepository
+import com.example.ecommerce.utils.BaseViewModel
+import com.example.ecommerce.utils.Observer
+import com.example.ecommerce.utils.singleHelper
+
+
+// Lớp AddFavoriteViewModel là một ViewModel trong kiến trúc MVVM được sử dụng để xử lý dữ liệu và logic liên quan đến hành động thêm sản phẩm vào danh sách yêu thích của người dùng.
+class AddCommentViewModel(private val addCommentRepository: AddCommentRepository, ) : BaseViewModel() {
+    // đối tượng MutableLiveData để truyền dữ liệu giữa lớp ViewModel và View (Activity hoặc Fragment)
+    val addCommentLiveData = MutableLiveData<Comment>()
+    // Phương thức để thêm sản phẩm vào danh sách yêu thích
+    fun addComment(id: Int,access_token: String,text:String) {
+        // Đặt giá trị của đối tượng progressbarLiveData là true để hiển thị tiến trình đang xử lý
+        progressbarLiveData.value = true
+        addCommentRepository.addComment(id,access_token,text)
+            .singleHelper()
+            // Không quan tâm đến kết quả trả về khi hành động kết thúc, đặt giá trị của progressbarLiveData là false để ẩn tiến trình
+            .doFinally {
+                progressbarLiveData.value = false
+            }
+            // Xử lý kết quả trả về khi thêm sản phẩm vào danh sách yêu thích thành công
+            .subscribe(object : Observer<Comment>(compositeDisposable) {
+                override fun onSuccess(t: Comment) {
+                    addCommentLiveData.value = t
+                }
+
+            })
+    }
+}
